113f6d168bc867f0d9bf74636cf171379dcfad0a
RozimuhammadIsomiddinov <roziisomiddinov@gmail.com>
2024-09-15 12:28:01 +0500

    changed origin

--- index.js
+++ index.js
@@ -43,8 +43,8 @@ const swaggerOptions = {
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
-app.use(express.json({ origin: "*" }));
-app.use(cors());
+app.use(express.json());
+app.use(cors({ origin: "*" }));
 app.use(express.urlencoded({ extended: true }));
 app.use("/public", express.static(path.resolve(__dirname, "public")));
 
