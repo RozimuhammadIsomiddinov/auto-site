d4ddcab99c06c69862618b01ca80359412d9bb40
RozimuhammadIsomiddinov <roziisomiddinov@gmail.com>
2024-09-15 14:40:30 +0500

    added time to moto

--- data/models/moto.js
+++ data/models/moto.js
@@ -70,9 +70,7 @@ const Motorcycle = sequelize.define(
       type: DataTypes.STRING,
     },
   },
-  {
-    timestamps: false,
-  }
+  { timestamps: true, underscored: false }
 );
 
 module.exports = Motorcycle;
