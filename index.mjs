4f6fb3a26ed109d09740377bb07e5adbfab61978
rozimukhammad <159604873+RozimuhammadIsomiddinov@users.noreply.github.com>
2024-10-17 19:47:28 +0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -82,6 +82,18 @@ if (!fs.existsSync(imagesFolderPath)) {
 } else {
   console.log("Images folder already exists within the public folder.");
 }
+const allowedOrigins = ["https://api.youcarrf.ru", "http://127.0.0.1:5173"];
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 app.use(express.json());
 app.use(
