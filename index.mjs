e808e79f80a5eef5c8dd0899331eaa129ed10016
rozimukhammad <159604873+RozimuhammadIsomiddinov@users.noreply.github.com>
2024-10-18 13:05:03 +0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -90,7 +90,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, token');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
