7bdf3d6b657237e0b47d992227cdbba535a4871b
RozimuhammadIsomiddinov <roziisomiddinov@gmail.com>
2025-02-01 21:11:15 +0500

    added

--- controllers/mark/getByIDMark.js
+++ controllers/mark/getByIDMark.js
@@ -5,7 +5,7 @@ const getByIDMark = async (req, res) => {
     const { id } = req.params;
     if (!id) return res.status(404).json({ message: "send a id" });
     const result = await Mark.findByPk(id);
-    res.status(200).json(result);
+    res.status(200).json(result.mark_name);
   } catch (err) {
     res.status(400).json({ message: "Error from getByID", error: err.message });
   }
