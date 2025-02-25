821c6ba1c84c2ac70e3a5dc29636cdf009a564e0
RozimuhammadIsomiddinov <roziisomiddinov@gmail.com>
2025-02-25 10:18:41 +0500

    added

---
+++ controllers/news/search.js
@@ -0,0 +1,23 @@
+const { Op } = require("sequelize");
+const Car = require("../../data/models/automobile");
+const Motorcycle = require("../../data/models/moto");
+const CommerceCar = require("../../data/models/commerce");
+
+const searchCont = async (req, res) => {
+  try {
+    const { q } = req.params;
+
+    const [cars, moto, commerce] = await Promise.all([
+      Car.findAll({ where: { model: { [Op.iLike]: `%${q}%` } } }),
+      Motorcycle.findAll({ where: { model: { [Op.iLike]: `%${q}%` } } }),
+      CommerceCar.findAll({ where: { model: { [Op.iLike]: `%${q}%` } } }),
+    ]);
+
+    return res.status(200).json({ cars, moto, commerce });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Server xatosi" });
+  }
+};
+
+module.exports = { searchCont };
