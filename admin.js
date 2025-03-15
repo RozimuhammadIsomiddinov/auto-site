327ac4a6cb0fead1dc0225276b63ce29cbc46533
RozimuhammadIsomiddinov <roziisomiddinov@gmail.com>
2025-03-15 22:08:48 +0500

    added

--- admin.js
+++ admin.js
@@ -1,6 +1,5 @@
 import AdminJS from "adminjs";
 import { dark, light, noSidebar } from "@adminjs/themes";
-
 import AdminJSExpress from "@adminjs/express";
 import { Database, Resource } from "@adminjs/sequelize";
 import { ComponentLoader } from "adminjs";
@@ -38,9 +37,7 @@ const uploadOptions = {
     url: ({ record }) => {
       const filePath = record?.params?.imagePath || record?.params?.image;
       if (!filePath) return null;
-
       const cleanedPath = filePath.replace(/^public\//, "");
-
       return `${process.env.BACKEND_URL}/${cleanedPath}`;
     },
   },
@@ -60,37 +57,76 @@ const uploadOptions = {
       `${process.env.BACKEND_URL}/${path.basename(filePath)}`,
   },
 };
-
-const commonProperties = {
-  id: { isVisible: { list: true, edit: false, filter: true, show: true } },
-  image: { isVisible: { list: true, edit: true, filter: true, show: true } },
-  country: { isVisible: { list: true, edit: true, filter: true, show: true } },
-  year: { isVisible: { list: true, edit: true, filter: true, show: true } },
-  cost: { isVisible: { list: true, edit: true, filter: true, show: true } },
-  milage: { isVisible: { list: true, edit: true, filter: true, show: true } },
-  engine: { isVisible: { list: true, edit: true, filter: true, show: true } },
-  volume: { isVisible: { list: false, edit: true, filter: true, show: true } },
-  horsepower: {
-    isVisible: { list: false, edit: true, filter: true, show: true },
+const componentOptions = {
+  year: {
+    label: "Год выпуска",
+    isVisible: { list: true, show: true, edit: false },
   },
-  drive: { isVisible: { list: false, edit: true, filter: true, show: true } },
+  cost: {
+    label: "Цена",
+    isVisible: { list: true, show: true, edit: false },
+  },
+  milage: {
+    label: "Пробег",
+    isVisible: { list: true, show: true, edit: false },
+  },
+  engine: {
+    label: "Двигатель",
+    isVisible: { list: true, show: true, edit: false },
+  },
+  volume: {
+    label: "Объем",
+    isVisible: { list: true, show: true, edit: false },
+  },
+  horsepower: {
+    label: "Лошадиные силы",
+    isVisible: { list: true, show: true, edit: false },
+  },
+  drive: {
+    label: "Привод",
+    isVisible: { list: true, show: true, edit: false },
+  },
+  checkpoint: {
+    label: "Коробка передач",
+    isVisible: { list: true, show: true, edit: false },
+  },
   statement: {
-    isVisible: { list: true, edit: true, filter: true, show: true },
+    label: "Состояние",
+    isVisible: { list: true, show: true, edit: false },
+  },
+  description: {
+    label: "Описание",
+    isVisible: { list: true, show: true, edit: false },
   },
   authoremail: {
-    isVisible: { list: true, edit: true, filter: true, show: true },
+    label: "Email автора",
+    isVisible: { list: true, show: true, edit: false },
   },
-  rate: { isVisible: { list: false, edit: true, filter: true, show: true } },
-  mark: { isVisible: { list: true, edit: true, filter: true, show: true } },
-  model: { isVisible: { list: true, edit: true, filter: true, show: true } },
-  createdAt: {
-    isVisible: { list: false, edit: false, filter: false, show: true },
+  rate: {
+    label: "Оплата",
+    isVisible: { list: true, show: true, edit: false },
   },
-  updatedAt: {
-    isVisible: { list: false, edit: false, filter: false, show: true },
+  seen: {
+    label: "Просмотры",
+    isVisible: { list: true, show: true, edit: false },
+  },
+  liked: {
+    label: "Лайки",
+    isVisible: { list: true, show: true, edit: false },
+  },
+  liked_user: {
+    label: "Понравилось пользователям",
+    isVisible: { list: true, show: true, edit: false },
+  },
+  mark: {
+    label: "Марка",
+    isVisible: { list: true, show: true, edit: false },
+  },
+  model: {
+    label: "Модель",
+    isVisible: { list: true, show: true, edit: false },
   },
 };
-
 const adminJs = new AdminJS({
   componentLoader,
   defaultTheme: dark.id,
@@ -99,44 +135,68 @@ const adminJs = new AdminJS({
     {
       resource: Car,
       options: {
-        properties: commonProperties,
-        navigation: { name: "информация", icon: "Database" },
+        properties: { ...componentOptions },
+        navigation: { name: "Автомобили" },
       },
     },
     {
       resource: Motorcycle,
       options: {
-        properties: commonProperties,
-        navigation: { name: "информация", icon: "Database" },
+        properties: { ...componentOptions },
+
+        navigation: { name: "Автомобили" },
       },
     },
     {
       resource: CommerceCar,
+
       options: {
-        properties: commonProperties,
-        navigation: { name: "информация", icon: "Database" },
+        properties: { ...componentOptions },
+
+        navigation: { name: "Автомобили", icon: "Truck" },
       },
     },
     {
       resource: Users,
       options: {
-        listProperties: ["id", "image", "name", "email", "role", "userrate"],
-        properties: { ...commonProperties, password: { isVisible: false } },
-        navigation: { name: "информация", icon: "Database" },
+        listProperties: ["id", "image", "name", "email", "role"],
+        properties: {
+          name: { label: "Имя пользователя" },
+          email: { label: "Электронная почта" },
+          password: { label: "Пароль" },
+          role: { label: "Роль" },
+          userrate: { label: "Тариф" },
+          password: { isVisible: false },
+        },
+        navigation: { name: "Пользователи", icon: "User" },
       },
     },
     {
       resource: Cart,
-      options: { navigation: { name: "информация", icon: "Database" } },
+      options: {
+        properties: {
+          user_id: { label: "Пользователь" },
+          product_id: { label: "Идентификатор товара" },
+          product_type: { label: "Тип товара" },
+          quantity: { label: "Количество" },
+          added_time: { label: "Дата добавления" },
+        },
+        navigation: { name: "информация", icon: "Database" },
+      },
     },
     {
       resource: News,
       options: {
         properties: {
+          title: { label: "Заголовок" },
+          content: { label: "Содержание" },
+          vehicle: { label: "Транспорт" },
+          author: { label: "Автор" },
           uploadedFile: {
             isVisible: { list: false, show: false, edit: true },
           },
           image: {
+            label: "Изображение",
             isVisible: { list: true, show: true, edit: false },
           },
         },
@@ -148,10 +208,13 @@ const adminJs = new AdminJS({
       resource: Mark,
       options: {
         properties: {
+          mark_name: { label: "Название марки" },
+
           uploadedFile: {
             isVisible: { list: false, show: false, edit: true },
           },
           image: {
+            label: "Изображение",
             isVisible: { list: true, show: true, edit: false },
           },
         },
@@ -166,7 +229,10 @@ const adminJs = new AdminJS({
           uploadedFile: {
             isVisible: { list: false, show: false, edit: true },
           },
+          title: { label: "Заголовок" },
+          subtitle: { label: "Подзаголовок" },
           image: {
+            label: "Изображение",
             isVisible: { list: true, show: true, edit: false },
           },
         },
@@ -178,40 +244,184 @@ const adminJs = new AdminJS({
       resource: Country,
       options: {
         properties: {
+          name: {
+            label: "Название",
+          },
           uploadedFile: {
             isVisible: { list: false, show: false, edit: true },
           },
           image: {
+            label: "Изображение",
             isVisible: { list: true, show: true, edit: false },
           },
+          description: {
+            label: "Описание",
+          },
         },
-        navigation: { name: "информация", icon: "Database" },
+        navigation: { name: "Страны", icon: "Globe" },
       },
       features: [uploadFileFeature({ ...uploadOptions, componentLoader })],
     },
     {
       resource: Offer,
-      options: { navigation: { name: "информация", icon: "Database" } },
-    },
-    {
-      resource: Chat,
-      options: { navigation: { name: "информация", icon: "Database" } },
+
+      options: {
+        properties: {
+          name: { label: "Имя" },
+          surname: { label: "Фамилия" },
+          phone: { label: "Телефон" },
+        },
+        navigation: { name: "Предложения", icon: "Gift" },
+      },
     },
   ],
   rootPath: "/admin",
   locale: {
+    language: "ru",
     translations: {
-      en: {
-        resources: {
-          Image: {
-            properties: {
-              file: "File",
-            },
+      labels: { navigation: "Навигация", dashboard: "Панель управления" },
+      messages: { welcome: "Добро пожаловать!", noRecords: "Нет записей" },
+      resources: {
+        Car: {
+          name: "Автомобили",
+          properties: {
+            mark: "Марка",
+            model: "Модель",
+            year: "Год выпуска",
+            cost: "Цена",
+            milage: "Пробег",
+            engine: "Двигатель",
+            volume: "Объем",
+            horsepower: "Лошадиные силы",
+            drive: "Привод",
+            checkpoint: "Коробка передач",
+            statement: "Состояние",
+            description: "Описание",
+            authoremail: "Email автора",
+            rate: "Оплата",
+            seen: "Просмотры",
+            liked: "Лайки",
+            liked_user: "Понравилось пользователям",
+          },
+        },
+        Motorcycle: {
+          name: "Мотоциклы",
+          properties: {
+            mark: "Марка",
+            model: "Модель",
+            year: "Год выпуска",
+            cost: "Цена",
+            milage: "Пробег",
+            engine: "Двигатель",
+            volume: "Объем",
+            horsepower: "Лошадиные силы",
+            drive: "Привод",
+            checkpoint: "Коробка передач",
+            statement: "Состояние",
+            description: "Описание",
+            authoremail: "Email автора",
+            rate: "Оплата",
+            seen: "Просмотры",
+            liked: "Лайки",
+            liked_user: "Понравилось пользователям",
+          },
+        },
+        CommerceCar: {
+          name: "Коммерческие автомобили",
+          properties: {
+            mark: "Марка",
+            model: "Модель",
+            year: "Год выпуска",
+            cost: "Цена",
+            milage: "Пробег",
+            engine: "Двигатель",
+            volume: "Объем",
+            horsepower: "Лошадиные силы",
+            drive: "Привод",
+            checkpoint: "Коробка передач",
+            statement: "Состояние",
+            description: "Описание",
+            authoremail: "Email автора",
+            rate: "Оплата",
+            seen: "Просмотры",
+            liked: "Лайки",
+            liked_user: "Понравилось пользователям",
+          },
+        },
+        Users: {
+          name: "Пользователи",
+          properties: {
+            name: "Имя пользователя",
+            email: "Электронная почта",
+            password: "Пароль",
+            role: "Роль",
+            userrate: "Тариф",
+          },
+        },
+        Cart: {
+          name: "Корзина",
+          properties: {
+            user_id: "Пользователь",
+            product_id: "Идентификатор товара",
+            product_type: "Тип товара",
+            quantity: "Количество",
+            added_time: "Дата добавления",
+          },
+        },
+        News: {
+          name: "Новости",
+          properties: {
+            title: "Заголовок",
+            content: "Содержание",
+            vehicle: "Транспорт",
+            author: "Автор",
+            image: "Изображение",
+          },
+        },
+        Mark: {
+          name: "Марки",
+          properties: {
+            mark_name: "Название марки",
+            image: "Изображение",
+          },
+        },
+        Country: {
+          name: "Страны",
+          properties: {
+            name: "Название",
+            image: "Изображение",
+            description: "Описание",
+          },
+        },
+        Offer: {
+          name: "Предложения",
+          properties: {
+            name: "Имя",
+            surname: "Фамилия",
+            phone: "Телефон",
+          },
+        },
+        Banner: {
+          name: "Баннеры",
+          properties: {
+            title: "Заголовок",
+            subtitle: "Подзаголовок",
+            image: "Изображение",
+          },
+        },
+        Chat: {
+          name: "Чаты",
+          properties: {
+            sender_id: "ID отправителя",
+            receiver_id: "ID получателя",
+            message: "Сообщение",
+            timestamp: "Время отправки",
           },
         },
       },
     },
   },
+
   branding: {
     companyName: "Auto Site",
     metaTitle: "Admin Dashboard",
@@ -220,7 +430,6 @@ const adminJs = new AdminJS({
     withMadeWithLove: false,
   },
 });
-
 const adminRouter = AdminJSExpress.buildAuthenticatedRouter(
   adminJs,
   {
