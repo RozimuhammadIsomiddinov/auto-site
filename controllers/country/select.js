e0992a2ddee4260a863d1da8980cded09a5c2b12
rozimukhammad <159604873+RozimuhammadIsomiddinov@users.noreply.github.com>
2025-03-18 16:01:11 +0500

    Update select.js

--- controllers/country/select.js
+++ controllers/country/select.js
@@ -1,12 +1,20 @@
 import Country from "../../data/models/country.js";
 
+const baseUrl = "https://api.youcarrf.ru/public/images/";
+
 export const getAllCountry = async (req, res) => {
   try {
     const result = await Country.findAll();
-    res.status(200).json(result);
+
+    const updatedResult = result.map((country) => {
+      return {
+        ...country,
+        image: country.image.startsWith("http") ? country.image : baseUrl + country.image,
+      };
+    });
+
+    res.status(200).json(updatedResult);
   } catch (e) {
-    res
-      .status(500)
-      .json({ message: "Error from getAllCountry", error: e.message });
+    res.status(500).json({ message: "Error from getAllCountry", error: e.message });
   }
 };
